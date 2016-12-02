a909573cb69d1de27c05019036ec7bab06204c3f
Soren H. Pedersen <speder13@student.aau.dk>
2016-12-02 05:56:33 +0100

    newest
    filer til builder

---
+++ bayesbuilder/bayesbuilder/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bayesbuilder
+{
+    public partial class Form1 : Form
+    {
+        string[] emptyImages;
+        string[] ballImages;
+        string[] errorImages;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void load_files()
+        {
+            emptyImages = Directory.GetFiles("images/Empty/", "*.*");
+            ballImages = Directory.GetFiles("images/Ball/", "*.*");
+            errorImages = Directory.GetFiles("images/Error/", "*.*");
+        }
+        
+    }
+}
